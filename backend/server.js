057f64788c254be6239e47286c67795d303b23cd
Jayshree Patra <patrajayshree2005@gmail.com>
2025-08-21 14:51:09 +0530

    Updates server.js with small fixes

--- backend/server.js
+++ backend/server.js
@@ -17,9 +17,9 @@ app.use("/api/notes",notesRoutes)
 
 const __dirname = path.resolve();
 
-if(process.env.Node_ENV === "production") {
+if(process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname,"/frontend/dist")));
-    app.get("/{*splat}", (req, res) => {
+    app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
     });
 }
